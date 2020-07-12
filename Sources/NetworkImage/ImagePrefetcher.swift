70c72838dade89d10e1fb7d6bd05981479e730b6
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-07-12 07:37:32 +0200

    Add ImagePrefetcher

---
+++ Sources/NetworkImage/ImagePrefetcher.swift
@@ -0,0 +1,31 @@
+#if canImport(UIKit) && canImport(Combine)
+    import Combine
+    import Foundation
+
+    @available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    public final class ImagePrefetcher {
+        private let session: URLSession
+        private var cancellables: [URL: AnyCancellable] = [:]
+
+        public init(session: URLSession = .imageLoading) {
+            self.session = session
+        }
+
+        public func prefetchImages(with urls: Set<URL>) {
+            assert(Thread.isMainThread)
+
+            for url in urls where !cancellables.keys.contains(url) {
+                cancellables[url] = session.dataTaskPublisher(for: url)
+                    .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
+            }
+        }
+
+        public func cancelPrefetchingImages(with urls: Set<URL>) {
+            assert(Thread.isMainThread)
+
+            for url in urls {
+                cancellables.removeValue(forKey: url)
+            }
+        }
+    }
+#endif
