7e8f4f6cb68f2663b3057b6aee14a7b53d5f872e
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-08-19 17:58:30 +0200

    Better handle default values in constructor

--- Sources/NetworkImage/SwiftUI/ResizableNetworkImageStyle.swift
+++ Sources/NetworkImage/SwiftUI/ResizableNetworkImageStyle.swift
@@ -27,53 +27,52 @@
 
     @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
     public struct ResizableNetworkImageStyle: NetworkImageStyle {
+        public enum Defaults {
+            #if os(iOS)
+                public static var backgroundColor: Color {
+                    Color(.secondarySystemBackground)
+                }
+
+                public static var foregroundColor: Color {
+                    Color(.systemFill)
+                }
+
+            #elseif os(macOS) || os(tvOS) || os(watchOS)
+                public static var backgroundColor: Color {
+                    .secondary
+                }
+
+                public static var foregroundColor: Color {
+                    Color.primary.opacity(0.5)
+                }
+            #endif
+
+            #if os(iOS) || os(tvOS) || os(watchOS)
+                public static var errorPlaceholder: Image? {
+                    Image(systemName: "photo")
+                }
+
+            #elseif os(macOS)
+                public static let errorPlaceholder: Image? = nil
+            #endif
+        }
+
         private let backgroundColor: Color
         private let foregroundColor: Color
         private let contentMode: ContentMode
         private let errorPlaceholder: Image?
 
         public init(
-            backgroundColor: Color,
-            foregroundColor: Color,
-            contentMode: ContentMode,
-            errorPlaceholder: Image?
+            backgroundColor: Color = Defaults.backgroundColor,
+            foregroundColor: Color = Defaults.foregroundColor,
+            contentMode: ContentMode = .fill,
+            errorPlaceholder: Image? = Defaults.errorPlaceholder
         ) {
             self.backgroundColor = backgroundColor
             self.foregroundColor = foregroundColor
             self.contentMode = contentMode
             self.errorPlaceholder = errorPlaceholder
         }
-
-        #if os(iOS)
-            public init(errorPlaceholder: Image? = Image(systemName: "photo")) {
-                self.init(
-                    backgroundColor: Color(.secondarySystemBackground),
-                    foregroundColor: Color(.systemFill),
-                    contentMode: .fill,
-                    errorPlaceholder: errorPlaceholder
-                )
-            }
-
-        #elseif os(tvOS) || os(watchOS)
-            public init(errorPlaceholder: Image? = Image(systemName: "photo")) {
-                self.init(
-                    backgroundColor: Color.secondary,
-                    foregroundColor: Color.primary.opacity(0.5),
-                    contentMode: .fill,
-                    errorPlaceholder: errorPlaceholder
-                )
-            }
-
-        #elseif os(macOS)
-            public init(errorPlaceholder: Image? = nil) {
-                self.init(
-                    backgroundColor: Color.secondary,
-                    foregroundColor: Color.primary.opacity(0.5),
-                    contentMode: .fill,
-                    errorPlaceholder: errorPlaceholder
-                )
-            }
-        #endif
 
         public func makeBody(configuration: NetworkImageConfiguration) -> some View {
             ZStack {
