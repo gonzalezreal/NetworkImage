414949807ebb1c83bc422e77d8fe05a7d400d22d
gonzalezreal <gonzalezreal@users.noreply.github.com>
2021-01-12 16:51:11 +0000

    Run swiftformat

--- Tests/NetworkImageTests/Fixtures.swift
+++ Tests/NetworkImageTests/Fixtures.swift
@@ -4,13 +4,13 @@ import Foundation
 enum Fixtures {
     // Photo by Charles Deluvio (https://unsplash.com/@charlesdeluvio)
     static let anyImageURL = fixtureURL("charles-deluvio-REtZm_TkolU-unsplash.jpg")
-    
+
     static let anyOtherImageURL = URL(string: "https://picsum.photos/id/237/300/200")!
     static let invalidImageURL = fixtureURL("unknown")
-    
+
     static let anyImageResponse = try! Data(contentsOf: anyImageURL)
     static let anyResponse = Data(base64Encoded: "Z29uemFsZXpyZWFs")!
-    
+
     static let anyImage = try! decodeImage(from: anyImageResponse)
     static let anyError = NetworkImageError.badStatus(500)
 }
