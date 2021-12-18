edae607e04f139d77f4eee509bebcf277f872d42
Guille Gonzalez <guillermo.gonzalez@me.com>
2021-12-18 21:44:04 +0100

    Add new initializers to NetworkImage and deprecate the old ones

--- Sources/NetworkImage/Internal/Deprecations.swift
+++
@@ -1,7 +0,0 @@
-import Combine
-import Foundation
-
-// NB: Deprecated in 4.0.0
-
-@available(*, deprecated, renamed: "PlatformImage")
-public typealias OSImage = PlatformImage
--- Sources/NetworkImage/Internal/Unavailable.swift
+++ Sources/NetworkImage/Internal/Unavailable.swift
@@ -3,6 +3,38 @@ import SwiftUI
 
 // NB: Unavailable in 4.0.0
 
+@available(
+  *,
+  unavailable,
+  message: "You can style a NetworkImage by providing a content closure in one of the initializers."
+)
+public protocol NetworkImageStyle {
+  associatedtype Body: View
+  func makeBody(configuration: Self.Configuration) -> Body
+  typealias Configuration = NetworkImageStyleConfiguration
+}
+
+@available(
+  *,
+  unavailable,
+  message: "You can style a NetworkImage by providing a content closure in one of the initializers."
+)
+public struct NetworkImageStyleConfiguration {
+  public var image: Image
+  public var size: CGSize
+}
+
+@available(
+  *,
+  unavailable,
+  message: "You can style a NetworkImage by providing a content closure in one of the initializers."
+)
+public struct ResizableNetworkImageStyle: NetworkImageStyle {
+  public func makeBody(configuration: Configuration) -> some View {
+    EmptyView()
+  }
+}
+
 extension EnvironmentValues {
   @available(
     *,
@@ -33,6 +65,16 @@ extension View {
     message: "You can use the 'transaction' parameter in NetworkImage to animate state changes."
   )
   public func networkImageScheduler(_ networkImageScheduler: UIScheduler) -> some View {
+    EmptyView()
+  }
+
+  @available(
+    *,
+    unavailable,
+    message:
+      "You can style a NetworkImage by providing a content closure in one of the initializers."
+  )
+  public func networkImageStyle<S>(_ networkImageStyle: S) -> some View where S: NetworkImageStyle {
     EmptyView()
   }
 }
